cca4d86b65f8e18eb8e74ed20d9c353d8c28fa78
Mercado-Daniel <daniel-jtag@hotmail.com>
2025-01-23 19:36:44 -0300

    Se creo el proyecto de back end para la tienda judas

---
+++ src/main/java/com/tiendajudas/Tienda/judas/TiendaJudasApplication.java
@@ -0,0 +1,13 @@
+package com.tiendajudas.Tienda.judas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJudasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJudasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendajudas/Tienda/judas/TiendaJudasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendajudas.Tienda.judas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaJudasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
